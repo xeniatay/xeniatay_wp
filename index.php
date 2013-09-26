be48ec821ee4208be500a00fa6eb4d85348dc708
Xenia Tay <xeniatzy@gmail.com>
2013-09-26 06:49:00 -0400

    Massive cleanup and fixed bugs and made responsive BETTARRR

--- index.php
+++ index.php
@@ -19,113 +19,111 @@
 			        </div>
 		        </li>
 	        </ul>
-	        <label class='workexp-cusec tl-content-left' for='cusec'>
+	        <label class='workexp-cusec tl-content tl-left' for='cusec'>
 	            <div class='workexp-date-loc tl-date-loc'>
-		            <p>2013 &mdash; August to Present</p>
+		            <p class='hide-mobile'>2013 &mdash; August to Present</p>
+		            <p class='show-mobile'>2013 &mdash; Aug to Present</p>
 		            <p class='workexp-location'>Montreal, Quebec</p>
 	            </div>
 	            <h2 class='workexp-company-name'>CUSEC 2014</h2>
 	            <p class='workexp-job-desc'>
-		            Currently volunteering as webmaster and designer for
-		            <br/>the annual Canadian University Software
-		            <br/>Engineering Conference.
+		            Currently volunteering as webmaster and designer for the annual Canadian University Software Engineering Conference.
 		            <a class='workexp-sample-link' href='http://2014.cusec.net' title='CUSEC 2014'>2014.cusec.net</a>
 	            </p>
 	            <a class='show-mobile workexp-sample-link' href='http://2014.cusec.net' title='CUSEC 2014'>2014.cusec.net</a>
 	            <div class='tl-divider'></div>
 	            <input class='tl-content-input' type='radio' name='workexp' id='cusec'/>
 	        </label>
-	        <label class='workexp-indochino tl-content-right' for='indochino'>
+	        <label class='workexp-indochino tl-content tl-right' for='indochino'>
 	            <div class='workexp-date-loc tl-date-loc'>
-		            <p>2013 &mdash; May to August</p>
+		            <p class='hide-mobile'>2013 &mdash; May to August</p>
+		            <p class='show-mobile'>2013 &mdash; May to Aug</p>
 		            <p class='workexp-location'>Vancouver, BC</p>
 	            </div>
 	            <h2 class='workexp-company-name'>Indochino</h2>
 	            <p class='workexp-job-desc'>
-	            	Co-op &mdash;
-		            Worked as a Front End Engineer on a
-		            <br/>two-person team, maintaining and building new features for the entire Indochino e-commerce platform
+	            	Co-op &mdash; Worked as a Front End Engineer on a two-person team, maintaining and building new features for the entire Indochino e-commerce platform
 		            <a class='workexp-sample-link' href='http://bit.ly/indoMDC' title='Indochino Modern Dandy Collection'>Live Example: Indochino's Modern Dandy Collection</a>
 	            </p>
 	            <a class='show-mobile workexp-sample-link' href='http://bit.ly/indoMDC' title='Indochino Modern Dandy Collection'>Live Example</a>
 	            <div class='tl-divider'></div>
 	            <input class='tl-content-input' type='radio' name='workexp' id='indochino'/>
 	        </label>
-	        <label class='workexp-mathorient tl-content-left' for='mathorient'>
+	        <label class='workexp-mathorient tl-content tl-left' for='mathorient'>
 	            <div class='workexp-date-loc tl-date-loc'>
-		            <p>2013 &mdash; April to September</p>
+		            <p class='hide-mobile'>2013 &mdash; April to September</p>
+		            <p class='show-mobile'>2013 &mdash; Apr to Sept</p>
 		            <p class='workexp-location'>University of Waterloo</p>
 	            </div>
 	            <h2 class='workexp-company-name'>
 	            	Math Orientation 2013
 	            </h2>
 	            <p class='workexp-job-desc'>
-	            	Volunteered as Web Coordinator and implemented the 2013 site. I also took on the role of Head Pink Tie
-	            	<br/>(head frontline leader) during Orientation Week.
+	            	Volunteered as Web Coordinator and implemented the 2013 site. I also took on the role of Head Pink Tie (head frontline leader) during Orientation Week.
 		            <a class='workexp-sample-link' href='http://orientation.math.uwaterloo.ca' title='Math Orientation Website'>orientation.math.uwaterloo.ca</a>
 	            </p>
 	            <a class='show-mobile workexp-sample-link' href='http://orientation.math.uwaterloo.ca' title='Math Orientation Website'>View Site</a>
 	            <div class='tl-divider'></div>
 	            <input class='tl-content-input' type='radio' name='workexp' id='mathorient'/>
 	        </label>
-	        <label class='workexp-unhold tl-content-right' for='unhold'>
+	        <label class='workexp-unhold tl-content tl-right' for='unhold'>
 	            <div class='workexp-date-loc tl-date-loc'>
-		            <p>2013 &mdash; January to April</p>
+		            <p class='hide-mobile'>2013 &mdash; January to April</p>
+		            <p class='show-mobile'>2013 &mdash; Jan to Apr</p>
 		            <p class='workexp-location'>Waterloo, ON</p>
 	            </div>
 	            <h2 class='workexp-company-name'>
 		            Unhold
 	            </h2>
 	            <p class='workexp-job-desc'>
-	            	Handled front end design and development for a
-	            	<br/>webapp service aimed to relieve the frustration
-	            	<br/>of being stuck on hold in a phone call.
+	            	Handled front end design and development for a webapp service aimed to relieve the frustration of being stuck on hold in a phone call.
 		            <a class='workexp-sample-link' href='http://unhold.me' title='Unhold'>unhold.me</a>
 	            </p>
 	            <a class='show-mobile workexp-sample-link' href='http://unhold.me' title='Unhold'>unhold.me</a>
 	            <div class='tl-divider'></div>
 	            <input class='tl-content-input' type='radio' name='workexp' id='unhold'/>
 	        </label>
-	        <label class='workexp-bv tl-content-left' for='bazaarvoice'>
+	        <label class='workexp-bv tl-content tl-left' for='bazaarvoice'>
 	            <div class='workexp-date-loc tl-date-loc'>
-		            <p>2012 &mdash; September to December</p>
+		            <p class='hide-mobile'>2012 &mdash; September to December</p>
+		            <p class='show-mobile'>2012 &mdash; Sept to Dec</p>
 		            <p class='workexp-location'>Austin, TX</p>
 	            </div>
 	            <h2 class='workexp-company-name'>BazaarVoice</h2>
 	            <p class='workexp-job-desc'>
 	            	Co-op &mdash;
-	            	Worked as a Software Engineer on
-	            	<br/>Conversations 2.0, as part of the team in charge
-	            	<br/>of rewriting Bazaarvoice's rating and review
-	            	<br/>system from the ground up.
+	            	Worked as a Software Engineer on Conversations 2.0, as part of the team in charge of rewriting Bazaarvoice's rating and review system from the ground up.
 		            <a class='workexp-sample-link' href='http://bit.ly/BVsample' title='Bazaarvoice Example Review'>Live Ratings/Reviews Example</a>
 	            </p>
 	            <a class='show-mobile workexp-sample-link' href='http://bit.ly/BVsample' title='Bazaarvoice Example Review'>Live Example</a>
 	            <div class='tl-divider'></div>
 	            <input class='tl-content-input' type='radio' name='workexp' id='bazaarvoice'/>
 	        </label>
-	        <label class='workexp-mathnews tl-content-right' for='mathnews'>
+	        <label class='workexp-mathnews tl-content tl-right' for='mathnews'>
 	            <div class='workexp-date-loc tl-date-loc'>
 		            <p>2012 &mdash; July</p>
 		            <p class='workexp-location'>
-					<span class='mn-math'>math</span><span class='mn-news'>NEWS</span>, UW's Math Student Newspaper
+					<span class='mn-math'>math</span><span class='mn-news'>NEWS</span>, <span class='mn-hide-ipad'>UW's</span> Math Student Newspaper
 		            </p>
 	            </div>
 	            <h2 class='workexp-company-name'>
 		            	A Comic in <span class='mn-math'>math</span><span class='mn-news'>NEWS</span>
 	            </h2>
 	            <p class='workexp-job-desc'>
-		            Inspired by a campus-wide game of Humans VS Zombies, I drew a comic about the dramatic events during the
-		            <br/>game &mdash; starring me as the main character.
-			    	<a class='workexp-sample-link' href='http://xeniatay.com/media/hvz1.jpg' title='mathNEWS comic'>Read the Comic</a>
+		            Inspired by a campus-wide game of Humans VS Zombies, I drew a comic about the dramatic events during the game &mdash; starring me as the main character.
+			    	<a class='workexp-sample-link' href='http://xeniatay.com/media/hvz1.jpg' title='mathNEWS comic'>
+				    	<img class='mn-comic-thumbnail' src='http://xeniatay.com/media/hvz1.jpg' alt='Comic Thumbnail'/>
+				    	<span>Read the Comic</span>
+			    	</a>
 	            </p>
-		    	<a class='show-mobile workexp-sample-link' href='http://xeniatay.com/media/hvz1.jpg' title='mathNEWS comic'>Read</a>
+		    	<a class='show-mobile workexp-sample-link' href='http://xeniatay.com/media/hvz1.jpg' title='mathNEWS comic'>Read the Comic</a>
 	            <div class='tl-divider'></div>
 	            <input class='tl-content-input' type='radio' name='workexp' id='mathnews'/>
 	        </label>
-	        <label class='workexp-velocity tl-content-left' for='velocity'>
+	        <label class='workexp-velocity tl-content tl-left' for='velocity'>
 	             <div class='workexp-date-loc tl-date-loc'>
-		            <p>2012 &mdash; May to August</p>
+		            <p class='hide-mobile'>2012 &mdash; May to August</p>
+		            <p class='show-mobile'>2012 &mdash; May to Aug</p>
 		            <p class='workexp-location'>
 			            University of Waterloo
 		            </p>
@@ -141,18 +139,16 @@
 	            <div class='tl-divider'></div>
 	            <input class='tl-content-input' type='radio' name='workexp' id='velocity'/>
 	        </label>
-	        <label class='workexp-watrhub tl-content-right' for='watrhub'>
+	        <label class='workexp-watrhub tl-content tl-right' for='watrhub'>
 	             <div class='workexp-date-loc tl-date-loc'>
-		            <p>2012 &mdash; January to April</p>
+		            <p class='hide-mobile'>2012 &mdash; January to April</p>
+		            <p class='show-mobile'>2012 &mdash; Jan to Apr</p>
 		            <p class='workexp-location'>Toronto, ON</p>
 	            </div>
 	            <h2 class='workexp-company-name'>WatrHub</h2>
 	            <p class='workexp-job-desc'>
-	            	Worked as the sole frontend developer and designer in a 5-person startup. Designed and implemented the
-	            	<br/>entire WatrHub platform which was released
-	            	<br/>to investors during Beta Launch.
+	            	Worked as the sole frontend developer and designer in a 5-person startup. Designed and implemented the entire WatrHub platform which was released to investors during Beta Launch.
 	            </p>
-	            <!-- <p class='workexp-desc'> Working at a 5-person startup and being a key player (only frontend developer and designer) really inspired me. Despite the constant crunchtimes, I was super dedicated to my work because of the inspiring payoff it led to. Inspired me to really drill down into frontend - learn how to write better code, learn how to improve. Self-taught a lot of good practices too. Google was my best friend. Loved the small team and tightly knit group and big responsibilities. </p> -->
 	            <div class='tl-divider'></div>
 	            <input class='tl-content-input' type='radio' name='workexp' id='watrhub'/>
 	        </label>
